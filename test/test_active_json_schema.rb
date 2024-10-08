3e7b63256fdae92fcc6e7eb699830b734a8fd7d1
Kaleb Lape <kaleb.lape@gmail.com>
2024-10-08 11:16:08 -0500

    bundle gem active_json_schema --exe --mit --test=minitest --ci=github --linter=standard

---
+++ test/test_active_json_schema.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class TestActiveJsonSchema < Minitest::Test
+  def test_that_it_has_a_version_number
+    refute_nil ::ActiveJsonSchema::VERSION
+  end
+
+  def test_it_does_something_useful
+    assert false
+  end
+end
---
+++ test/test_helper.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+$LOAD_PATH.unshift File.expand_path("../lib", __dir__)
+require "active_json_schema"
+
+require "minitest/autorun"
